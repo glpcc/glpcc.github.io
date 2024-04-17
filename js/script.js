9a382a8a011fdd73e8f20fcc4d7b45fa185dfc51
Gonzalo Lope Carrasco <64266133+glpcc@users.noreply.github.com>
2024-04-17 18:34:05 +0200

    perfected the portfolio

--- js/script.js
+++ js/script.js
@@ -16,3 +16,17 @@ function changeToEnglish() {
         element.style.display = "none";
     });
 }
+
+function ShowMoreProjects() {
+    document.querySelectorAll(".less_important").forEach(element => {
+        element.style.display = "block";
+    });
+    document.getElementById("btn-projects-more").style.display = "none";
+};
+
+function ShowLessProjects() {
+    document.querySelectorAll(".less_important").forEach(element => {
+        element.style.display = "none";
+    });
+    document.getElementById("btn-projects-more").style.display = "block";
+}
